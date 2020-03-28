b31982a1c3da693acf90afe68f6e478c09339cee
jchojna <jakub.chojna@gmail.com>
2020-03-28 14:07:00 +0100

    feat(Cell): add remove button with basic functionality

--- src/components/App.js
+++ src/components/App.js
@@ -34,6 +34,12 @@ class App extends Component {
     this.togglePopup();
   }
 
+  removeCell = (id) => {
+    this.setState(prevState => ({
+      cells: prevState.cells.filter(cell => cell.id !== id)
+    }));
+  }
+
   render() {
     const { isPopupVisible, cells } = this.state;
 
@@ -47,8 +53,15 @@ class App extends Component {
           {/* LIST OF CELLS */}
           {
             cells.map(cell => {
+              const { id, category, value } = cell;
               return (
-                <Cell key={cell.id}/>
+                <Cell
+                  key={id}
+                  id={id}
+                  category={category}
+                  value={value}
+                  onRemove={this.removeCell}
+                />
               );
             })
           }
@@ -57,7 +70,13 @@ class App extends Component {
             className="button button--add button--main"
             onClick={this.togglePopup}
           >
-            +
+            <svg
+              className="button__svg"
+              viewBox="0 0 40 40"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <polyline points="0,15 0,25 15,25 15,40 25,40 25,25 40,25 40,15 25,15 25,0 15,0 15,15" />
+            </svg>
           </button>
           {/* POPUP */}
           {
