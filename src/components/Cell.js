6cf90f3f55872167aa77bb81d5afc72dbc34da76
jchojna <jakub.chojna@gmail.com>
2020-03-28 11:22:23 +0100

    feat: show popup window on 'plus' button click

---
+++ src/components/Cell.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../scss/Cell.scss';
+
+class Cell extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="Cell">
+        Cell
+      </div>
+    );
+  }
+}
+export default Cell;
